82d9074afb0811e181b7226c36957ddc8a7cedd7
Mario Essig <mario..a.essig@gmail.com>
2020-09-22 21:16:39 -0500

    add question prompt

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+/*
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
 const fs = require('fs');
@@ -7,3 +21,4 @@ fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw new Error(err);
     console.log('Portfolio complete! Checkout index.html to see the output!')
 });
+*/
