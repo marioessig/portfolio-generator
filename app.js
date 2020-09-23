652392772eda4de56e60d4feafbe4fc61b32da00
Mario Essig <mario..a.essig@gmail.com>
2020-09-22 20:20:32 -0500

    modularize HTML

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('===============');
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Checkout index.html to see the output!')
+});
